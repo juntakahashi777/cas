48ba3023dd20b77d81d0983549ec527738d4b291
Jun Takahashi <juntakahashi777@gmail.com>
2016-03-05 17:13:02 -0500

    - added cassandra database integration: - show database contents (naive) on get request - update database with form post

--- cas.py
+++ cas.py
@@ -1,21 +1,34 @@
 from flask import Flask, request
 from jinja2 import Environment, PackageLoader
+from cassandra.cluster import Cluster
 
 app = Flask(__name__)
+cluster = Cluster()
+session = cluster.connect('mykeyspace')
+msgid = 0
+
 
 @app.route("/", methods=['GET', 'POST'])
-def test():
+def route_main():
+    global msgid
     def render_controlpanel():
+        data = session.execute('SELECT * from users')
         env = Environment(loader=PackageLoader('cas', 'templates'))
         template = env.get_template('controlpanel.jinja.html')
-        return template.render()
+        return template.render(data=data)
         
     if request.method == 'GET':
         return render_controlpanel()
     else:
-        
+        msgid += 1
+        input_a = request.form['input_a']
+        input_b = request.form['input_b']
+        print input_a, input_b
+        session.execute(
+            '''INSERT INTO users (user_id, fname, lname) 
+            values (%s, %s, %s)''', (msgid, input_a, input_b))
         return render_controlpanel()
 
 if __name__ == "__main__":
     app.debug = True
-    app.run("0.0.0.0", 5000)
+    app.run(host="0.0.0.0", port=3400)
